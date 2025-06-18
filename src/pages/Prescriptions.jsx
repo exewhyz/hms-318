112567b44634ea0c59aa8e7a2668362e1a364a2f
Aniket Raj <araj52444@gmail.com>
2025-06-18 08:03:45 +0000

    added prescription

--- src/pages/Prescriptions.jsx
+++ src/pages/Prescriptions.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
 
 export default function Prescriptions() {
+
+    const prescriptions = [
+        {
+            id: 1,
+            doctor: "Dr. Abhishek",
+            details: "bidjhffhmugik"
+        },
+        {
+            id: 2,
+            doctor: "Dr. Harshvardhan",
+            details: "mugidsjfdsk"
+        },
+        {
+            id: 3,
+            doctor: "Dr. Akarshan",
+            details: "urefhojpe'wl"
+        }
+    ]
+
+
     return (
-        <div>Prescriptions</div>
+        <div className=''>
+            <h1 className='font-bold text-4xl'>Precriptions</h1>
+            <div className='flex flex-col gap-4 mt-2'>
+                {
+                    prescriptions.map((pres) => {
+                        return (
+                            <div className='flex gap-4 border-b-2 last:border-none'>
+                                <p className=''>Doctor: {pres.doctor}</p>
+                                <p>Details: {pres.details}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
