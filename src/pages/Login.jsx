e4a5679f0fb7062227a86bddd7adc7eac402d209
Aniket Raj <araj52444@gmail.com>
2025-06-18 07:06:22 +0000

    added submit function in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Login() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Logged In")
+    }
+
     return (
         <div className='h-full flex items-center justify-center'>
-            <form className='p-6 bg-blue-100/30 rounded-lg flex gap-4 flex-col items-center justify-center w-90 h-60'>
+            <form onSubmit={handleSubmit} className='p-6 bg-blue-100/30 rounded-lg flex gap-4 flex-col items-center justify-center w-90 h-60'>
                 <h2 className='font-bold text-2xl'>Login</h2>
                 <input type="email" placeholder='email@example.com' className='border border-gray-400 p-2 w-full rounded' />
                 <input type="password" placeholder='password' className='border border-gray-400 p-2 w-full rounded' />
-                <button className='px-4 py-1 w-full text-white rounded bg-blue-500 '>Login</button>
+                <button
+                    type='submit'
+                    className='px-4 py-1 w-full text-white rounded bg-blue-500 '
+                >
+                    Login
+                </button>
                 <Link to={"/register"} className='text-sm text-gray-400 hover:text-gray-500'>
                     Not have an Account, Register!
                 </Link>
