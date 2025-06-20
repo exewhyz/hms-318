3e6b5eb113723a5657fc26cc3ee0d503b01ecfc5
Aniket Raj <araj52444@gmail.com>
2025-06-20 05:08:12 +0000

    auth completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import Navbar from "./components/Navbar.jsx"
 import Booking from './pages/Booking.jsx';
 import Prescriptions from './pages/Prescriptions.jsx';
+import { useAuth } from './context/authContext.jsx';
 
 export default function App() {
+  const { isAuthenticated } = useAuth();
   return (
     <>
       <Navbar />
@@ -15,11 +17,20 @@ export default function App() {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/appointment-booking' element={<Booking />} />
-          <Route path='/prescriptions' element={<Prescriptions />} />
+          <Route
+            path='/'
+            element={isAuthenticated ? <Home /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path='/appointment-booking'
+            element={isAuthenticated ? <Booking /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path='/prescriptions'
+            element={isAuthenticated ? <Prescriptions /> : <Navigate to={"/login"} />}
+          />
         </Routes>
-      </main>
+      </main> :
     </>
   )
 }
