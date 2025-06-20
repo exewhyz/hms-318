0623a47a6fdd3e9877862db0e2c65ba77fd07797
Aniket Raj <araj52444@gmail.com>
2025-06-20 04:53:44 +0000

    added logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useAuth } from "../context/authContext"
 
 
 export default function Navbar() {
+    const { logout, isAuthenticated } = useAuth();
     return (
         <header>
             <nav className='flex h-14 px-4 py-2 bg-amber-100 items-center justify-between'>
@@ -15,15 +17,26 @@ export default function Navbar() {
                     <Link to={"/appointment-booking"}>Appointment</Link>
                     <Link to={"/prescriptions"}>Prescriptions</Link>
                 </div>
-                <div className='flex gap-4'>
-                    <Link className='bg-blue-400 px-4 py-2 rounded-lg ' to={"login"}>
-                        Login
-                    </Link>
-                    <Link className='bg-green-400 px-4 py-2 rounded-lg' to={"/register"}>
-                        Register
-                    </Link>
-                </div>
-            </nav>
-        </header>
+                {
+                    isAuthenticated ? (
+                        <div>
+                            <button onClick={logout} className='bg-red-500/90 hover:bg-red-500 text-white px-4 py-2 rounded-lg'
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    ) : (
+                        <div className='flex gap-4'>
+                            <Link className='bg-blue-400 px-4 py-2 rounded-lg ' to={"login"}>
+                                Login
+                            </Link>
+                            <Link className='bg-green-400 px-4 py-2 rounded-lg' to={"/register"}>
+                                Register
+                            </Link>
+                        </div>
+                    )
+                }
+            </nav >
+        </header >
     )
 }
